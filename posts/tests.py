d3df94ea533fd2740ba337521171fcf35fa427f0
omariut <omarcpgcbl@gmail.com>
2021-07-09 00:17:36 +0600

    test file added

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class PostModelTest(Post):
+    def setUp(self):
+        post=Post.objects.get(text='second post')
+    def test_post_model(self):
+        post=Post.objects.get(id=2)
+        text='second post'
+        self.assertEqual(text, f"{post.text}")
